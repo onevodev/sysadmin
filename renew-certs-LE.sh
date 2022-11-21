3acfb66c14e51fdba2399708e2312b6bf518fceb
mtf <m.ferrante@onevo.it>
2022-11-21 11:35:12 +0100

    removed service restart, that has to be done manually

--- renew-certs-LE.sh
+++ renew-certs-LE.sh
@@ -16,7 +16,7 @@ done
 ufw allow from 0.0.0.0/0 to any port 80 proto tcp comment "certbot"
 
 # execute renewal
-certbot renew && sv restart mosquitto
+certbot renew
 
 # delete ufw rule
 echo y | ufw delete $(ufw status numbered |(grep 'certbot'|awk -F"[][]" '{print $2}'))
